9c6cd388f0d77cb0f4f1c9086e20a898789d397c
Felipe Menezes <felipe.menezes@supersim.com.br>
2022-07-24 11:44:15 -0300

    Api for parking control made with Java and Spring integrating with PostgreSQL

---
+++ src/main/java/com/api/parkingcontrol2/ParkingControl2Application.java
@@ -0,0 +1,13 @@
+package com.api.parkingcontrol2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkingControl2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkingControl2Application.class, args);
+	}
+
+}
